e5d9f27baf2aa852f619f0d7f45584498aa81a1b
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-28 20:47:37 +0530

    created the single user item fn

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -175,5 +175,26 @@ contract NFTMarketplace is ERC721URIStorage{
                         }
                      return items;
                     }
+
+                    //SINGLE USER ITEM
+                    function fetchItemsListed()public view returns(MarketItem[] memory){
+                        uint256 totalCount = _tokenIds.current();
+                        uint256 itemCount = 0;
+                        uint256 currentIndex =0;
+
+                        for (uint256 i = 0; i < totalCount; i++){
+                            if(idMarketItem[i + 1].owner == msg.sender){
+                                itemCount +=1;
+                            }
+                        }
+                        MarketItem[] memory items = new MarketItem[](itemCount);
+                        for(uint 256 i = 0; i < totalCount; i++){
+                            uint256 currentId = i +1;
+
+                            MarketItem storage currentItem = idMarketItem[currentId];
+                            items[currentIndex] = currentItem;
+                            currentIndex += 1;
+                        }
                 }
+                return items
 }
